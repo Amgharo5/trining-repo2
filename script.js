8910cab5f640a17f8dfedf0d8099de9a3935a291
Amgharo5 <aitbenamgharm@gmail.com>
2023-08-11 03:27:49 +0100

    practicing-Filter-and-map

--- script.js
+++ script.js
@@ -1,22 +1,63 @@
-let swappingCases = "aMGHaR";
-let invertedNumbers = [1, -50, 10, -100, -200];
-let ingoreNumbers = "ait125be5namgha85r";
+// map function
 
-let sw = swappingCases
+// let swappingCases = "aMGHaR";
+// let invertedNumbers = [1, -50, 10, -100, -200];
+// let ingoreNumbers = "ait125be5namgha85r";
+
+// let sw = swappingCases
+//   .split("")
+//   .map((e) => (e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase()))
+//   .join("");
+// console.log(sw);
+
+// let invert = invertedNumbers.map(function (e) {
+//   return -e;
+// });
+// console.log(invert);
+
+// let ing = ingoreNumbers
+//   .split("")
+//   .map(function (e) {
+//     return isNaN(parseInt(e)) ? e : "";
+//   })
+//   .join("");
+// console.log(ing);
+
+// Filter function
+let friends = ["Ahmed", "Taher", "Arwa", "Anouar", "Said", "Maroua"];
+
+let filteredFriends = friends.filter((el) => el.startsWith("A"));
+console.log(filteredFriends);
+
+let numbers = [15, 12, 1, 2, 153, 1521];
+
+let evenNumbers = numbers.filter((el) => el % 2 === 0);
+
+console.log(evenNumbers);
+
+// practicing filter function
+
+let santence = "I love home and good restevly words like sleeping";
+
+let smallWords = santence
+  .split(" ")
+  .filter((el) => el.length <= 4)
+  .join(" ");
+console.log(smallWords);
+
+let justString = "amg120har4o";
+let fliterString = justString
   .split("")
-  .map((e) => (e === e.toUpperCase() ? e.toLowerCase() : e.toUpperCase()))
+  .filter((el) => isNaN(parseInt(el)))
   .join("");
-console.log(sw);
+console.log(fliterString);
 
-let invert = invertedNumbers.map(function (e) {
-  return -e;
-});
-console.log(invert);
+// using map and filter togither
 
-let ing = ingoreNumbers
+let namee = "A12BC52D521";
+let justNumbers = namee
   .split("")
-  .map(function (e) {
-    return isNaN(parseInt(e)) ? e : "";
-  })
+  .filter((el) => parseInt(el))
+  .map((el) => el * el)
   .join("");
-console.log(ing);
+console.log(justNumbers);
